be83f825e6f39eb083b10ec6af95b7178c17fb05
Andea Balducci <mtb.snowboard@gmail.com>
2016-11-26 00:17:59 +0100

    added insert sample

---
+++ 01-insert.js
@@ -0,0 +1,14 @@
+// mongo mongodb://localhost/demo 01-insert.js
+
+db.users.insert({
+    "name": "Andrea Balducci",
+    "phone": "555-000-1234",
+    "address": {
+        "line1": "Via M. della Libertà 3",
+        "line2": "Proximo srl",
+        "city": "Castelfidardo",
+        "zipcode": NumberInt(60022),
+        "country": "IT"
+    },
+    "age": NumberInt(42)
+})
